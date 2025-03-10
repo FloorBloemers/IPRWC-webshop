5158070b0d2835991efa993302dbf5ac48257efd
Floor Bloemers <floorbloemers1@gmail.com>
2025-03-10 21:56:56 +0100

    feat: added simple ngIf navigation

--- iprwc-webshop/src/app/app.component.ts
+++ iprwc-webshop/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'iprwc-webshop';
+
+  loadedFeature = 'recipe';
+
+  onNavigate(feature:string) {
+    this.loadedFeature = feature;
+  }
 }
