8fec174df0939d0620d16db216f1bb6ecb9fe4a1
Floor Bloemers <floorbloemers1@gmail.com>
2025-03-13 02:01:24 +0100

    feat: added shopping list service

--- iprwc-webshop/src/app/recipes/recipe.service.ts
+++ iprwc-webshop/src/app/recipes/recipe.service.ts
@@ -1,15 +1,25 @@
 import {Recipe} from "./recipe.model";
-import {EventEmitter} from "@angular/core";
+import {EventEmitter, Injectable} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe("A Test Recipe", "This is simply a Test", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505"),
-    new Recipe("Another Test Recipe", "This is simply a Test", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505")
+    new Recipe("Schnitzel", "This is simply a Test", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505", [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]),
+    new Recipe("Burger", "This is simply a Test", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505", [new Ingredient('Buns', 2), new Ingredient('Meat', 1)])
   ];
+
+  constructor(private slService: ShoppingListService) {
+  }
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
