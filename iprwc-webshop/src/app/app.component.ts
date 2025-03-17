6eaea8cee16b86fb67942dd4b02c31476dbe6d93
Floor Bloemers <floorbloemers1@gmail.com>
2025-03-17 22:08:16 +0100

    feat: added forms

--- iprwc-webshop/src/app/app.component.ts
+++ iprwc-webshop/src/app/app.component.ts
@@ -7,10 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'iprwc-webshop';
-
-  loadedFeature = 'recipe';
-
-  onNavigate(feature:string) {
-    this.loadedFeature = feature;
-  }
 }
