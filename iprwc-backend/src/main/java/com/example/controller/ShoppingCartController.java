c66dd75d2c11c5ff64111c96038453d7a4de8a07
Floor Bloemers <floorbloemers1@gmail.com>
2025-10-25 23:27:40 +0200

    feat: added CORS config

--- iprwc-backend/src/main/java/com/example/controller/ShoppingCartController.java
+++ iprwc-backend/src/main/java/com/example/controller/ShoppingCartController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/carts")
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class ShoppingCartController {
     private final ShoppingCartService service;
 
