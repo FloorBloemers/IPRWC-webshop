1a66aaa955df98b42d4709e813b18d1d7ffe8c25
Floor Bloemers <floorbloemers1@gmail.com>
2025-12-04 19:12:39 +0100

    added yml and dockerfile for digital ocean

--- iprwc-backend/src/main/java/com/example/repository/ShoppingCartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repositories;
-
-import com.example.models.ShoppingCartModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ShoppingCartRepository extends JpaRepository<ShoppingCartModel, Long> {
-}
