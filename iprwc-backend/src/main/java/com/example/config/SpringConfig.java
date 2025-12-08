5853c70fb53a2b2a18425fe65ae195e8a40efb0a
Floor Bloemers <floorbloemers1@gmail.com>
2025-12-08 10:32:00 +0100

    dependency error

--- iprwc-backend/src/main/java/com/example/config/SpringConfig.java
+++ iprwc-backend/src/main/java/com/example/config/SpringConfig.java
@@ -2,8 +2,6 @@ package com.example.config;
 
 import com.example.daos.userDAO;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
