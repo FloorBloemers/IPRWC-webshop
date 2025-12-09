7b59b55dbf957c9e24d13ae9b32a0fe3e8fc8143
Floor Bloemers <floorbloemers1@gmail.com>
2025-12-09 14:42:02 +0100

    changed role get

--- iprwc-backend/src/main/java/com/example/config/JwtAuthenticationFilter.java
+++ iprwc-backend/src/main/java/com/example/config/JwtAuthenticationFilter.java
@@ -16,6 +16,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import io.jsonwebtoken.Claims;
+
 
 import java.io.IOException;
 import java.util.List;
